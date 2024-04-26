a939de7deae1194216d6aef0172a07272e17cd2a
hjamttes <92005067+hjamttes@users.noreply.github.com>
2024-04-25 22:03:23 -0400

    Uploading every single fucking file

---
+++ src/setup/constants.ts
@@ -0,0 +1,6 @@
+export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
+export const DISCORD_LINK = "https://discord.gg/B9pQT5rjj9";
+export const GITHUB_LINK = "https://github.com/404";
+export const DONATION_LINK = "https://ko-fi.com/movieweb";
+export const GA_ID = import.meta.env.VITE_GA_ID;
+export const BACKEND_URL = "https://mw-backend.lonelil.ru";
